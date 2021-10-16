25bdab418b48ab9bdbd475219d83417e15eeb54e
Boubacar Sidy Diallo <bsdiallo07@gmail.com>
2021-10-16 13:41:09 +0200

    Initial cli

--- src/gnakrydev/main.py
+++ src/gnakrydev/main.py
@@ -1,11 +1,53 @@
 import argparse
+import json 
+import os
+import docker
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+# TODO THE TODOLIST
+# TODO Notifier
+# parser = argparse.ArgumentParser(description='Process some integers.')
+# parser.add_argument('integers', metavar='N', type=int, nargs='+',
+#                     help='an integer for the accumulator')
+# parser.add_argument('--sum', dest='accumulate', action='store_const',
+#                     const=sum, default=max,
+#                     help='sum the integers (default: find the max)')
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+# args = parser.parse_args()
+# print(args.accumulate(args.integers))
+
+
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tz", help="The timezone")
+    parser.add_argument(
+        "-r",
+        "--repeat",
+        help="number of times to repeat the greeting",
+        default=1,
+        type=int,
+    )
+    parser.add_argument(
+        "-i",
+        "--interval",
+        help="time in seconds between iterations",
+        default=3,
+        type=int,
+    )
+    args = parser.parse_args()
+    
+    # greet(args.tz, args.repeat, args.interval)
+    
+    print(os.getenv('topic'))
+    
+    
+    ### Docker info and cli
+    try:
+        client = docker.from_env()
+        for image in client.images.list():
+            print(image.id)
+    except:
+        print("An exception occurred")
+    
+    username = input("Enter username:")
+    print("Username is: " + username)
+    
