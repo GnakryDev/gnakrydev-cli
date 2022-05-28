7ba5f9429bf9450bbd969bff1b8e5e42c7264214
Boubacar Sidy Diallo <bsdiallo07@gmail.com>
2022-05-28 22:25:21 +0200

    Set messages and health

--- src/gnakrydev/main.py
+++ src/gnakrydev/main.py
@@ -2,15 +2,15 @@ import argparse
 import json
 import requests
 import yaml
-
+import uuid
 # Allow request for self signed https certificates
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-def health_check(gnakrydev_yml_file, apiKey):
-    gdev_broker_url = "https://broker-api.gnakrydev.com/webhooks/message?apiKey=" + apiKey
-    with open(gnakrydev_yml_file) as file:
+def health_check(params):
+    gdev_broker_url = "https://broker-api.gnakrydev.com/webhooks/message?apiKey=" + params.apikey
+    with open(params.config) as file:
         endpoint_list = yaml.load(file, Loader=yaml.FullLoader)
         for endpoint in endpoint_list["endpoints"]:
             response = requests.get(endpoint["url"], verify=False)
@@ -18,20 +18,64 @@ def health_check(gnakrydev_yml_file, apiKey):
                 payload = {"id": endpoint["url"], "type": "success",
                            "title": endpoint["name"], "category": "healthCheck"}
                 requests.post(gdev_broker_url, data=json.dumps(payload))
+                if params.verbose:
+                    print(endpoint["name"] + " => " +
+                          endpoint["url"] + ":  🆗 ✅ 😀")
             else:
                 payload = {"id": endpoint["url"], "type": "error",
                            "title": endpoint["name"], "category": "healthCheck"}
                 requests.post(gdev_broker_url, data=json.dumps(payload))
+                if params.verbose:
+                    print(endpoint["name"] + " => " +
+                          endpoint["url"] + ":  ❌ 😱 🚨")
+
+
+def send_message(params):
+
+    gdev_broker_url = "https://broker-api.gnakrydev.com/webhooks/message?apiKey=" + params.apikey
+    if params.type:
+        message_type = params.type
+    else:
+        message_type = "info"
+    payload = {"id": str(uuid.uuid4()), "type": message_type,
+               "title": params.title, "message": params.content, "category": "message"}
+    requests.post(gdev_broker_url, data=json.dumps(payload))
 
 
 def cli():
-    parser = argparse.ArgumentParser(description='Gnakrydev-cli client app')
+    parser = argparse.ArgumentParser(description='Gnakrydev-cli  app')
+    subparser = parser.add_subparsers(dest='command')
+    health = subparser.add_parser('health')
+    message = subparser.add_parser('message')
 
     # GnakryDev YML config file
-    parser.add_argument('--config', type=str, required=True)
+    health.add_argument('--config', type=str,
+                        required=True, help="YML config file")
 
-    # apiKey available on the mobile-app
-    parser.add_argument('--apikey', type=str, required=True)
+    # ApiKey available on the mobile-app
+    health.add_argument('--apikey', type=str, required=True,
+                        help="apiKey available on the mobile-app")
+
+    #
+    health.add_argument('--verbose', action='store_true',
+                        help="Show request details in stdout")
+    message.add_argument('--apikey', type=str, required=True,
+                         help="apiKey available on the mobile-app")
+    #
+    message.add_argument('--title', type=str,
+                         required=True, help="Message title")
+
+    # Message content
+    message.add_argument('--content', type=str,
+                         required=True, help="Message content")
+
+    # Message type
+    message.add_argument('--type', type=str,
+                         help="Message type:  info, warning, success, error. Default= info")
 
     args = parser.parse_args()
-    health_check(args.config, args.apikey)
+
+    if args.command == 'health':
+        health_check(args)
+    elif args.command == 'message':
+        send_message(args)
