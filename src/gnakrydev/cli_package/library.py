9003e74dcaee665635f715d7f3fe472f360cee2b
Boubacar Sidy Diallo <bsdiallo07@gmail.com>
2022-06-17 20:56:47 +0200

    cleannup and refactoring

--- src/gnakrydev/cli_package/library.py
+++ src/gnakrydev/cli_package/library.py
@@ -1,8 +1,21 @@
 import feedparser
 import json
 import requests
+import argparse
+import feedparser
+import json
+import requests
+import yaml
+import uuid
+import logging
+import sys
+from .Konstants import VERSION
 
-VERSION = "0.1.1"
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+logger = logging.getLogger(__name__)
 
 
 def app_version():
@@ -10,12 +23,10 @@ def app_version():
 
 
 def ping_mobile_app(params):
-    gdev_broker_url = "https://broker-api.gnakrydev.com/webhooks/cmd?apiKey=" + params.apikey
     message_type = "ping"
     message_id = "cmd"
-
     payload = {"id": message_id, "type": message_type}
-    requests.post(gdev_broker_url, data=json.dumps(payload))
+    webhook_message(params.apikey, payload)
 
 
 def cve_feed():
@@ -25,3 +36,41 @@ def cve_feed():
         print("-------------------------------------------------")
         print(feed.title)
         print(feed.link)
+
+
+def health_check(params):
+    with open(params.config) as file:
+        endpoint_list = yaml.load(file, Loader=yaml.FullLoader)
+        for endpoint in endpoint_list["endpoints"]:
+            response = requests.get(endpoint["url"], verify=False)
+            if response.status_code == 200:
+                payload = {"id": endpoint["url"], "type": "success",
+                           "title": endpoint["name"], "category": "healthCheck"}
+                webhook_message(params.apikey, payload)
+                logger.debug(endpoint["name"] + " => " +
+                             endpoint["url"] + ":  🆗 ✅ 😀")
+                if params.verbose:
+                    print(endpoint["name"] + " => " +
+                          endpoint["url"] + ":  🆗 ✅ 😀")
+            else:
+                payload = {"id": endpoint["url"], "type": "error",
+                           "title": endpoint["name"], "category": "healthCheck"}
+                webhook_message(params.apikey, payload)
+                logger.debug(endpoint["name"] + " => " +
+                             endpoint["url"] + ":  ❌ 😱 🚨")
+                if params.verbose:
+                    print(endpoint["name"] + " => " +
+                          endpoint["url"] + ":  ❌ 😱 🚨")
+
+
+def send_message(params):
+    message_type = params.type if params.type else "info"
+    message_id = params.id if params.id else str(uuid.uuid4())
+    payload = {"id": message_id, "type": message_type,
+               "title": params.title, "message": params.content, "category": "message"}
+    webhook_message(params.apikey, payload)
+
+
+def webhook_message(apikey, payload):
+    gdev_broker_url = "https://broker-api.gnakrydev.com/webhooks/message?apiKey=" + apikey
+    requests.post(gdev_broker_url, data=json.dumps(payload))
